affff53e1f33b40512fbb822644b851bc2c6760a
Joel D <imaginethere.snoie@gmail.com>
2021-10-05 22:12:25 +0300

    Made sign in and sing up page

---
+++ src/pages/sign-in-and-up/sing-in-and-up.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in';
+
+import './sing-in-and-up.scss';
+
+const SingInAndUpPage = () => (
+  <div className='sing-in-and-sign-up'>
+    <SignIn/>
+  </div>
+);
+
+export default SingInAndUpPage;

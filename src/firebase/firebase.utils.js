488ce3b9dfe994b28acdf66a7aa66f4b0618d05e
Joel D <imaginethere.snoie@gmail.com>
2021-11-09 20:09:15 +0200

    Fix firebase-config import

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import {firebaseConfig} from "../../.firebase-config";
+import {firebaseConfig} from "../.firebase-config";
 
 const config = {
   apiKey: firebaseConfig.apiKey,
